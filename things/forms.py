ae08d76db608ee5eaeb747676a34436502ca498f
Muhammad Islam <muhammad.islam@kcl.ac.uk>
2023-10-23 09:50:46 +0100

    Create Things form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,14 @@
 """Forms of the project."""
 
-# Create your forms here.
+from django import forms
+from .models import Thing
+
+class ThingsForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': forms.Textarea(), 'quantity': forms.NumberInput()}
+
+    # name = forms.CharField(label="Name")
+    # description = forms.CharField(widget=forms.Textarea, label="Description")
+    # quantity = forms.IntegerField(widget=forms.NumberInput, label="Quantity")
--- things/views.py
+++ things/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
-
+from .forms import ThingsForm
 def home(request):
-    return render(request, 'home.html', {'form': form})
+    return render(request, 'home.html')
+
+def things_form(request):
+    form = ThingsForm()
+    return render(request, 'thing.html', {'form': form})
