2e4f6caace984546d663fb8111b8f4a827f055f4
Muhammad Islam <muhammad.islam@kcl.ac.uk>
2023-11-22 10:33:03 +0000

    Added form to home page

--- things/views.py
+++ things/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from .forms import ThingForm
 def home(request):
-    return render(request, 'home.html')
-
-def things_form(request):
     form = ThingForm()
-    return render(request, 'thing.html', {'form': form})
+    return render(request, 'home.html', {'form': form})
